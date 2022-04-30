fixed bootstrap sampling but less data by a factor of 10

alf/algorithms/algorithm.py
@@ -1371,7 +1371,8 @@ class Algorithm(AlgorithmInterface):
                     experience, batch_info = self._replay_buffer.get_batch(
                         batch_size=(mini_batch_size *
                                     config.num_updates_per_train_iter),
-                        batch_length=config.mini_batch_length)
+                        batch_length=config.mini_batch_length,
+                        index=self._bootstrap_index)
                     num_updates = 1
             return experience, batch_info, num_updates, mini_batch_size
 
@@ -1505,8 +1506,9 @@ class Algorithm(AlgorithmInterface):
                 # error: https://github.com/pytorch/pytorch/issues/59756
                 indices = alf.nest.utils.convert_device(
                     torch.randperm(batch_size, device='cpu'))
+            if self._bootstrap:
+                indices = None
             for b in range(0, batch_size, mini_batch_size):
-
                 is_last_mini_batch = (u == num_updates - 1
                                       and b + mini_batch_size >= batch_size)
                 do_summary = (is_last_mini_batch
@@ -1522,7 +1524,6 @@ class Algorithm(AlgorithmInterface):
                                             mini_batch_size,
                                             update_counter_every_mini_batch,
                                             do_summary)
-
                 exp, train_info, loss_info, params = self._update(
                     mini_batch_list[0],
                     mini_batch_info_list[0],
@@ -1578,7 +1579,7 @@ class Algorithm(AlgorithmInterface):
 
         length = alf.nest.get_nest_size(experience, dim=1)
         mini_batch_length = (mini_batch_length or length)
-        if not whole_replay_buffer_training:
+        if not whole_replay_buffer_training and not self._bootstrap:
             assert mini_batch_length == length, (
                 "mini_batch_length (%s) is "
                 "different from length (%s). Not supported." %
@@ -1633,10 +1634,17 @@ class Algorithm(AlgorithmInterface):
                 # mini_batch_length, the temporal correlation can be better
                 # captured.
                 pass
-
-        experience = alf.nest.map_structure(
-            lambda x: x.reshape(-1, mini_batch_length, *x.shape[2:]),
-            experience)
+        # import pdb; pdb.set_trace()
+        # if self._bootstrap:
+        #     #if bootstrap, the second dimension needs to have mini_batch_length * num_agents shape
+        #     import pdb; pdb.set_trace()
+        #     experience = alf.nest.map_structure(
+        #         lambda x: x.reshape(-1, mini_batch_length * self._bootstrap_index.shape[0], *x.shape[2:]),
+        #         experience)
+        else:
+            experience = alf.nest.map_structure(
+                lambda x: x.reshape(-1, mini_batch_length, *x.shape[2:]),
+                experience)
 
         batch_size = alf.nest.get_nest_batch_size(experience)
 
@@ -1848,6 +1856,7 @@ class Algorithm(AlgorithmInterface):
                 batch = alf.nest.map_structure(lambda x: x[batch_indices],
                                                experience)
                 if batch_info:
+                    import pdb; pdb.set_trace()
                     binfo = alf.nest.map_structure(
                         lambda x: x[batch_indices] if isinstance(
                             x, torch.Tensor) else x, batch_info)

alf/algorithms/multiagent_one_step_loss.py
@@ -15,7 +15,8 @@ class MultiAgentOneStepTDLoss(TDLoss):
                  gamma: Union[float, List[float]] = 0.99,
                  td_error_loss_fn: Callable = losses.element_wise_squared_loss,
                  debug_summaries: bool = False,
-                 name: str = "OneStepTDLoss"):
+                 bootstrap: bool = False,
+                 name: str = "MultiAgentOneStepTDLoss"):
         """
         Args:
             gamma: A discount factor for future rewards. For
@@ -33,9 +34,10 @@ class MultiAgentOneStepTDLoss(TDLoss):
             debug_summaries=debug_summaries,
             td_lambda=0.95,
             name=name)
+        self._bootstrap = bootstrap
 
     def forward(self, info: namedtuple, value: torch.Tensor,
-                target_value: torch.Tensor, noise: None):
+                target_value: torch.Tensor, noise=None, bootstrap=False):
         """Calculate the loss.
 
         The first dimension of all the tensors is time dimension and the second
@@ -56,6 +58,7 @@ class MultiAgentOneStepTDLoss(TDLoss):
         Returns:
             LossInfo: with the ``extra`` field same as ``loss``.
         """
+        bootstrap = self._bootstrap or bootstrap
         returns = torch.zeros((target_value.shape[0]-1, target_value.shape[1], target_value.shape[2]))
         if target_value.shape == info.reward.shape:
             info = info._replace(reward = info.reward[:, :, 0])
@@ -105,7 +108,12 @@ class MultiAgentOneStepTDLoss(TDLoss):
         if noise != None:
             returns = returns + noise[:-1, :, :]
         loss = self._td_error_loss_fn(returns.detach(), value)
-        loss = torch.squeeze(loss.sum(dim=-1))
+        if bootstrap:
+            loss = torch.squeeze(torch.diagonal(loss, dim1=-2, dim2=-1))
+        else:
+            loss = torch.squeeze(loss.sum(dim=-1))
+        
+        import pdb; pdb.set_trace()
 
         if loss.ndim == 3:
             # Multidimensional reward. Average over the critic loss for all dimensions

alf/algorithms/seedtd_algorithm.py
@@ -87,6 +87,8 @@ class SeedTDAlgorithm(OffPolicyAlgorithm):
         else:
             bootstrap_index = None
 
+        self._bootstrap = bootstrap
+
         super().__init__(observation_spec=observation_spec,
                          action_spec=action_spec,
                          train_state_spec=SeedTDState(),
@@ -239,7 +241,6 @@ class SeedTDAlgorithm(OffPolicyAlgorithm):
 
     def calc_loss(self, info: SeedTDInfo):
         loss_fn = MultiAgentOneStepTDLoss(gamma=self._gamma, debug_summaries=True)
-
         gaussian_noise = info.noise
         if self._config.num_parallel_agents == 1:
             returns = value_ops.one_step_discounted_return(
@@ -260,7 +261,7 @@ class SeedTDAlgorithm(OffPolicyAlgorithm):
             # loss = element_wise_squared_loss(returns, info.new_value)
             # loss = loss.sum(dim=-1)
 
-            loss = loss_fn(info, value=info.value, target_value=info.target_value, noise=gaussian_noise)            
+            loss = loss_fn(info, value=info.value, target_value=info.target_value, noise=gaussian_noise, bootstrap=self._bootstrap)            
 
         """info.value, returns after extending, and loss should be the same shape"""
         if self._debug_summaries:

alf/algorithms/td.py
@@ -35,7 +35,7 @@ class TD(OffPolicyAlgorithm):
                  v=0.01,
                  gamma=0.99,
                  epsilon_greedy=0.1,
-                 max_episodic_reward=0,
+                 max_episodic_reward=100,
                  bootstrap=False,
                  env=None,
                  optimizer=None,
@@ -79,6 +79,7 @@ class TD(OffPolicyAlgorithm):
             bootstrap_index = torch.sort(bootstrap_index, dim=-1).values
         else:
             bootstrap_index = None
+        self._bootstrap = bootstrap
         super().__init__(observation_spec=observation_spec,
                          action_spec=action_spec,
                          train_state_spec=SeedTDState(),
@@ -178,21 +179,23 @@ class TD(OffPolicyAlgorithm):
         rollout_action = rollout_action.transpose(0, 1)
         value = value.gather(dim=-1, index=rollout_action.unsqueeze(2))
         value = torch.squeeze(value)
+        import pdb; pdb.set_trace()
 
         return AlgStep(output=action,
                        state=SeedTDState(),
                        info=SeedTDInfo(discount=inputs.discount,
-                                  step_type=inputs.step_type,
+                                  step_type=inputs.env_id,
                                   action=action,
                                   reward=inputs.reward,
                                   target_value=target_value,
                                   value=value))
 
     def calc_loss(self, info: SeedTDInfo):
+        import pdb; pdb.set_trace()
 
         if self._config.num_parallel_agents > 1: 
             loss_fn = MultiAgentOneStepTDLoss(gamma=self._gamma, debug_summaries=True)
-            loss = loss_fn(info, value=info.value, target_value=info.target_value, noise=None)            
+            loss = loss_fn(info, value=info.value, target_value=info.target_value, noise=None, bootstrap=self._bootstrap)            
         else:
             loss_fn = OneStepTDLoss(gamma=self._gamma, debug_summaries=True)
             loss = loss_fn(info=info, value=torch.squeeze(info.value), target_value=info.target_value)

alf/experience_replayers/replay_buffer.py
@@ -410,14 +410,23 @@ class ReplayBuffer(RingBuffer):
             start_pos = info.positions
             env_ids = info.env_ids
 
-            idx = start_pos.reshape(-1, 1)  # [B, 1]
-            idx = self.circular(
-                idx + torch.arange(batch_length).unsqueeze(0))  # [B, T]
-            out_env_ids = env_ids.reshape(-1, 1).expand(
-                batch_size, batch_length)  # [B, T]
+            ##if bootstrap, agent takes the diagonal when training so they have to sample experience from other agents' buffers
+            ##we assume a shared replay buffer means one observation per agent. idx // 10 is the index and idx % 10 is the env_id
+            if index != None:
+                idx = start_pos
+                t = idx #used for debug
+                out_env_ids = idx % index.shape[0]
+                idx = idx // index.shape[0]
+            else:
+                idx = start_pos.reshape(-1, 1)  # [B, 1]
+                idx = self.circular(
+                    idx + torch.arange(batch_length).unsqueeze(0))  # [B, T]
+                out_env_ids = env_ids.reshape(-1, 1).expand(
+                    batch_size, batch_length)  # [B, T]
             result = alf.nest.map_structure(lambda b: b[(out_env_ids, idx)],
                                             self._buffer)
-
+            # ind = [(i, j) for i in range(500) for j in range(500) if idx[0, i] == idx[0, j] and out_env_ids[0, i] == out_env_ids[0, j] and i != j]
+            import pdb; pdb.set_trace()
             if alf.summary.should_record_summaries():
                 alf.summary.scalar(
                     "replayer/" + self._name + ".original_reward_mean",
@@ -491,17 +500,31 @@ class ReplayBuffer(RingBuffer):
 
         if index != None:
             ##num_index is the number of elements in the index of each agent less than current size
-            num_index = (index <= self._current_size[0] - d).sum(dim=1)
-            pos = (r * num_index[env_ids]).to(torch.int64)
-            if batch_size_per_env > 1:
-                pos = torch.reshape(pos, (-1, index.shape[0]))
-                store = torch.zeros(pos.shape)
-                for i in range(pos.shape[0]):
-                    store[i, :] = index.gather(dim=1, index=pos[i, :].unsqueeze(dim=1)).squeeze()
-                pos = store.reshape(env_ids.shape)
-            else:
-                pos = index.gather(dim=1, index=pos.unsqueeze(dim=1)).squeeze()
-            pos += (self._current_pos - num_positions - batch_length + 1)[env_ids]
+            #(self._current_size[0] - d)*num_index since there are num_index agents each with a buffer
+            num_index = (index <= self.total_size-10).sum(dim=1)
+            # num_index = (index <= self._current_size[0] - d).sum(dim=1)
+            if batch_length > 1:
+                # #makes pos dimension [num_updates * num_agent, batch_length * num_agent]
+                ##assert batch_length == 2
+                r = torch.rand(env_ids.shape[0]//index.shape[0]//batch_length, index.shape[0])
+                pos = (r * num_index[env_ids][:batch_size_per_env//2].reshape(-1, index.shape[0]).repeat(index.shape[0], 1)).to(torch.int64)
+                #pos becomes a [batch_size, num_agents] tensor
+                #TODO: can simplify second part to num_index.repeat
+                #TODO: currently only takes [batch_size, batch_length * num_agents] experience, check if need factor of ten
+                store = torch.zeros((pos.shape[0], pos.shape[1]*2))
+                for i in range(store.shape[1]):
+                    if i % 2 == 0:
+                        #if even index gather from bootstrap index
+                        store[:, i] = index[i//2, :].gather(dim=0, index=pos[:, i//2])
+                    else:
+                        #if odd index then set to consecutive observation
+                        store[:, i] = (store[:, i-1] + 10*torch.ones((store[:, i-1].shape))).to(torch.int64) #makes second row the consecutive element
+                pos = store
+                import pdb; pdb.set_trace()
+            else: 
+                r = torch.rand(*env_ids.shape)
+                pos = (r * num_index[env_ids]).to(torch.int64)
+                pos += (self._current_pos - num_positions - batch_length + 1)[env_ids]
             pos = pos.type(torch.LongTensor)
         else:
             pos = (r * num_positions[env_ids]).to(torch.int64)